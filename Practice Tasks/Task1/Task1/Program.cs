6caaf031b32a47ab21c872da86df4709f19ab57d
Sergei Shalamow <serezha.schalamow@yandex.ru>
2023-11-28 20:54:49 +0500

    feat: implemented task solution

---
+++ Practice Tasks/Task1/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task1
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            List<string> testStrings = new List<string>() { "a", "abcdef", "abcde", "school", "maxim", "tech", "draniki so smetanoi"};
+
+            foreach (string str in testStrings)
+            {
+                Console.WriteLine($"{StringReverser.SplitAndReverse(str)} is even: {str.Length % 2 == 0}");
+            }
+        }
+    }
+}
---
+++ Practice Tasks/Task1/Task1/StringReverser.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1
+{
+    public static class StringReverser
+    {
+        public static string SplitAndReverse(string str)
+        {
+            if (str.Length % 2 == 0)
+            {
+                int stringLength = str.Length / 2;
+                string firstPart = str.Substring(0, stringLength).ReverseString();
+                string secondPart = str.Substring(stringLength, stringLength).ReverseString();
+                
+                return new string(firstPart + secondPart);
+            }
+            return str.Insert(0, str.ReverseString());
+        }
+    }
+}

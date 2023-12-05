ceeedade512ec4b9446f699b418d7ec12c3f7094
Sergei Shalamow <serezha.schalamow@yandex.ru>
2023-12-05 16:56:18 +0500

    feat: added sort type choosing

--- Practice Tasks/Task1/Task1/Program.cs
+++ Practice Tasks/Task1/Task1/Program.cs
@@ -8,8 +8,15 @@ namespace Task1
         {
             Console.Write("Введите строку: ");
             string input = Console.ReadLine();
-          
-            StringModifier modifier = new StringModifier(input, SortingMode.QuickSort);
+            Console.WriteLine("Введите тип сортировки (1 - QuickSort/2 - TreeSort): ");
+            SortingMode sortingMode = (SortingMode)Int32.Parse(Console.ReadLine());
+
+            if (!Enum.GetNames(typeof(SortingMode)).Any(e => e.Equals(sortingMode.ToString())))
+            {
+                throw new NullReferenceException("Enum value that given wasn't found in SortingMode");
+            }
+            
+            StringModifier modifier = new StringModifier(input, sortingMode);
             
             Console.WriteLine(modifier.Result);
             Console.WriteLine($"Максимальная строка, где края - гласные: {modifier.MaxVowelString}");

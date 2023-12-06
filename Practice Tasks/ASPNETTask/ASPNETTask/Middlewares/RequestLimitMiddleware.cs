80fa9275c4611c60bd3a06f4f7d806b8d69e77ce
Sergei Shalamow <serezha.schalamow@yandex.ru>
2023-12-07 04:35:09 +0500

    feat: implemented request limit middleware

---
+++ Practice Tasks/ASPNETTask/ASPNETTask/Middlewares/RequestLimitMiddleware.cs
@@ -0,0 +1,43 @@
+﻿namespace ASPNETTask.Middlewares
+{
+    public class RequestLimitMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly SemaphoreSlim _semaphore;
+        private readonly int _parallelLimit;
+        
+        private int _currentRequests = 0;
+
+        public RequestLimitMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _parallelLimit = configuration.GetValue<int>("Settings:ParallelLimit");
+            _semaphore = new SemaphoreSlim(0, _parallelLimit);
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _semaphore.WaitAsync();
+
+                if (IsLimitReached())
+                {
+                    context.Response.StatusCode = 503;
+                    await context.Response.WriteAsync($"HTTP ошибка 503 Service Unavailable. Достигнут лимит одновременных запросов {_currentRequests}.");
+                    return;
+                }
+                Interlocked.Increment(ref _currentRequests);
+                
+                await _next(context);
+            }
+            finally
+            {
+                _semaphore.Release();
+                Interlocked.Decrement(ref _currentRequests);
+            }
+        }
+        
+        private bool IsLimitReached() => _currentRequests > _parallelLimit;
+    }
+}
--- Practice Tasks/ASPNETTask/ASPNETTask/Program.cs
+++ Practice Tasks/ASPNETTask/ASPNETTask/Program.cs
@@ -1,4 +1,5 @@
 using ASPNETTask;
+using ASPNETTask.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<RequestLimitMiddleware>();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();

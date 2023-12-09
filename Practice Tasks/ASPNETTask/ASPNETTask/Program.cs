6531ac46376a1c60240049041481772320e2036c
Sergei Shalamow <serezha.schalamow@yandex.ru>
2023-12-10 00:56:20 +0500

    feat: changed sorting enum view from int to str

--- Practice Tasks/ASPNETTask/ASPNETTask/Program.cs
+++ Practice Tasks/ASPNETTask/ASPNETTask/Program.cs
@@ -1,15 +1,19 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services
+    .AddControllersWithViews() // or AddControllers() in a Web API
+    .AddJsonOptions(options => 
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
